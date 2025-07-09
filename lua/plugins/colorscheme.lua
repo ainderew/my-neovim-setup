6e03c5ce20e2f9bedb10b00d0a350f2d079ad3ec
Andrew Pinon <pinon@theoriamedical.com>
2025-07-10 04:25:10 +0800

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,36 @@
 return {
-  { "navarasu/onedark.nvim" },
-
   {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "onedark",
-      transparent = false,
-
-      -- styles = {
-      --   sidebars = "transparent",
-      --   floats = "transparent",
-      -- },
-    },
+    "navarasu/onedark.nvim",
+    priority = 1000,
+    -- Lazy-load the colourscheme only at start-up, not on every :colorscheme
+    lazy = false,
+    config = function()
+      require("onedark").setup({
+        style = "deep", -- <- the magic line
+        transparent = false, -- set true if you want a transparent bg
+        term_colors = true,
+        code_style = {
+          comments = "italic",
+          keywords = "bold",
+          functions = "none",
+        },
+        diagnostics = { darker = true },
+      })
+      require("onedark").load()
+    end,
   },
+  -- { "navarasu/onedark.nvim" },
+  --
+  -- {
+  --   "LazyVim/LazyVim",
+  --   opts = {
+  --     colorscheme = "onedark",
+  --     transparent = false,
+  --
+  --     -- styles = {
+  --     --   sidebars = "transparent",
+  --     --   floats = "transparent",
+  --     -- },
+  --   },
+  -- },
 }
