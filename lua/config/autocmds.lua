3021fb46bd4dfe6fa1e4a8304e0ef341c653a328
Andrew Pinon <pinon@theoriamedical.com>
2026-02-03 05:56:41 +0800

    trying to add auto detection of file changes made by ai agent

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,14 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+---@diagnostic disable: undefined-global
+
+-- Trigger `checktime` when files change on disk
+-- This ensures Neovim picks up changes made by Claude Code instantly
+vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
+  command = "checktime",
+})
+
+-- Notification when file is reloaded (optional quality of life)
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+  callback = function()
+    vim.notify("File changed on disk. Buffer reloaded.", vim.log.levels.INFO)
+  end,
+})
--- lua/plugins/dealwithit.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "AndrewRadev/dealwithit.vim",
-  keys = {
-    { "<leader>dwi", "<cmd>Dealwithit<CR>", desc = "Deal with it" },
-  },
-}
