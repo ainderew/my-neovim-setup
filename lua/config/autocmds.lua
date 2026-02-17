d2fdcb37a05a4b3847afd373461786f963f1c184
Andrew Pinon <andrewapinon@gmail.com>
2026-02-18 06:16:45 +0800

    theme update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,3 +18,20 @@ vim.api.nvim_create_autocmd("FileChangedShellPost", {
 
 -- Reduce CursorHold delay for faster file change detection (100ms)
 vim.opt.updatetime = 100
+
+-- Open image files with viu, outputting directly to iTerm2
+vim.api.nvim_create_autocmd("BufReadCmd", {
+  pattern = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.bmp", "*.ico" },
+  callback = function(args)
+    local file = vim.fn.fnamemodify(args.file, ":p")
+    vim.cmd("bdelete!")
+    vim.cmd("silent !viu " .. vim.fn.shellescape(file))
+  end,
+})
+
+-- Disable LSP diagnostics in terminal buffers (prevents underlines in Claude Code)
+vim.api.nvim_create_autocmd("TermOpen", {
+  callback = function(args)
+    vim.diagnostic.enable(false, { bufnr = args.buf })
+  end,
+})
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,36 +1,11 @@
 return {
+  { "catppuccin/nvim", name = "catppuccin" },
+  { "navarasu/onedark.nvim", name = "onedark" },
+
   {
-    "navarasu/onedark.nvim",
-    priority = 1000,
-    -- Lazy-load the colourscheme only at start-up, not on every :colorscheme
-    lazy = false,
-    config = function()
-      require("onedark").setup({
-        style = "deep", -- <- the magic line
-        transparent = false, -- set true if you want a transparent bg
-        term_colors = true,
-        code_style = {
-          comments = "italic",
-          keywords = "bold",
-          functions = "none",
-        },
-        diagnostics = { darker = true },
-      })
-      require("onedark").load()
-    end,
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
   },
-  -- { "navarasu/onedark.nvim" },
-  --
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "onedark",
-  --     transparent = false,
-  --
-  --     -- styles = {
-  --     --   sidebars = "transparent",
-  --     --   floats = "transparent",
-  --     -- },
-  --   },
-  -- },
 }
